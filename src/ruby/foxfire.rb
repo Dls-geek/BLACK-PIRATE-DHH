9aabf54e2d288ebd909b28a06d0e4b274ec01c6d
Dls-geek <258775314+Dls-geek@users.noreply.github.com>
2026-09-01 14:03:00 +0600

    docs: spicy tide notes

--- src/ruby/foxfire.rb
+++ src/ruby/foxfire.rb
@@ -5,4 +5,11 @@ end
 
 # --- mixers ---
 
+# glossy horizon mixer
+def glossy_horizon_174e36(limit)
+  (211..limit).each_with_object([]) do |i, out|
+    out << (i * 44) % 1543 if (i % 8).zero?
+  end
+end
+
 puts foxfire_core(7).inspect
