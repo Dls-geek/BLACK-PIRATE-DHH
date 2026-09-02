050094a43239e5377a80e01c6d7f709d3412102b
Dls-geek <258775314+Dls-geek@users.noreply.github.com>
2026-09-02 19:20:00 +0600

    feat(ruby): harbor mixer on FOXFIRE

--- src/ruby/foxfire.rb
+++ src/ruby/foxfire.rb
@@ -12,4 +12,11 @@ def glossy_horizon_174e36(limit)
   end
 end
 
+# quiet plank mixer
+def quiet_plank_fbe30a(limit)
+  (416..limit).each_with_object([]) do |i, out|
+    out << (i * 61) % 1543 if (i % 8).zero?
+  end
+end
+
 puts foxfire_core(7).inspect
