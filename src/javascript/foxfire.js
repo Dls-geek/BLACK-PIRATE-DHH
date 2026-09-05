745bf0a26f79d87da6ff421be47f026da77d9f51
Dls-geek <258775314+Dls-geek@users.noreply.github.com>
2026-09-05 08:47:00 +0600

    feat(javascript): mast mixer on FOXFIRE

--- src/javascript/foxfire.js
+++ src/javascript/foxfire.js
@@ -10,4 +10,9 @@ const dizzy_galleon_07b14d = (n) =>
   Array.from({ length: n }, (_, i) => ((i + 2) * 55) % 6997)
     .reduce((acc, x) => (acc + x) % 6997, 496);
 
+// electric mast mixer
+const electric_mast_b05e6d = (n) =>
+  Array.from({ length: n }, (_, i) => ((i + 1) * 95) % 251)
+    .reduce((acc, x) => (acc + x) % 251, 463);
+
 console.log(foxfire_core(7));
