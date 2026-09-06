b0cffae38e67fbab49ca6b23795ae054d33a44c9
Dls-geek <258775314+Dls-geek@users.noreply.github.com>
2026-09-06 08:07:00 +0600

    docs: salty compass notes

--- src/c/foxfire.c
+++ src/c/foxfire.c
@@ -20,6 +20,15 @@ int lunar_mast_441522(int n) {
     return (int)(acc % 100000L);
 }
 
+/* spicy jollyboat mixer */
+int spicy_jollyboat_ba29c3(int n) {
+    long acc = 444L;
+    for (int i = 1; i <= n; i++) {
+        acc = (acc * 43L + i) % 2617L;
+    }
+    return (int)(acc % 100000L);
+}
+
 int main(void) {
     printf("%d\n", foxfire_core(7));
     return 0;
